a78bbf0285565eaa6cb91fd29507499e2053be00
rg3915 <rg3915@yahoo.com.br>
2020-05-30 00:04:08 -0300

    Update settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,22 +79,16 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-# DATABASES = {
-#     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER', 'postgres'),
+        'PASSWORD': config('DATABASE_PASSWORD', 'postgres'),
+        'HOST': config('DATABASE_HOST', 'localhost'),
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -128,15 +122,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-# My config
-USE_DOCKER = True
-
 # CELERY
-if USE_DOCKER:
-    CELERY_BROKER_URL = 'redis://redis:6379'  # Docker
-else:
-    CELERY_BROKER_URL = 'redis://localhost:6379'  # Local
-
+CELERY_BROKER_URL = 'redis://redis:6379'  # Docker
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
