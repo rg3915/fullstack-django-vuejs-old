84aed39c4fc27e8bd2ee5066900bcac5f9bfc399
rg3915 <rg3915@yahoo.com.br>
2020-05-30 00:04:17 -0300

    Update env_gen.py

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -7,20 +7,21 @@ from django.utils.crypto import get_random_string
 
 
 chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+password = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
 CONFIG_STRING = """
 DEBUG=True
 SECRET_KEY=%s
 ALLOWED_HOSTS=127.0.0.1, .localhost
-#DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
-#DEFAULT_FROM_EMAIL=
-#EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
-#EMAIL_HOST=
-#EMAIL_PORT=
-#EMAIL_USE_TLS=
-#EMAIL_HOST_USER=
-#EMAIL_HOST_PASSWORD=
-""".strip() % get_random_string(50, chars)
+DATABASE_NAME=django_celery_db
+DATABASE_USER=myuser
+DATABASE_PASSWORD=%s
+DATABASE_HOST=localhost
+APP_ID=xyz
+KEY=xyz
+SECRET=xyz
+CLUSTER=xyz
+""".strip() % (get_random_string(50, chars), get_random_string(32, password))
 
 # Writing our configuration file to '.env'
 with open('.env', 'w') as configfile:
